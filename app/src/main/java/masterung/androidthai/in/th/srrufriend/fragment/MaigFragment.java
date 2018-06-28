7645aad5525061b6dd65b4bda733e4db38bc2a08
bugpoppy <bugpoppy@hotmail.com>
2018-06-28 16:35:36 +0700

    Finish 28June 04.35PM

--- app/src/main/java/masterung/androidthai/in/th/srrufriend/fragment/MaigFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/srrufriend/fragment/MaigFragment.java
@@ -1,12 +1,15 @@
 package masterung.androidthai.in.th.srrufriend.fragment;
 
+import android.icu.text.Replaceable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import masterung.androidthai.in.th.srrufriend.R;
 
@@ -17,6 +20,21 @@ public class MaigFragment extends Fragment{
         super.onActivityCreated(savedInstanceState);
 
 //        Register Controller
+
+        TextView textVieW = getView().findViewById(R.id.txtNewRegister);
+        textVieW.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                Replaceable Fragment
+                getActivity()
+                        .getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMaintFragment,new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
 
     } // Main Method
 
